c555cc1e49a16cb2303a93fbff43025e8bd12f73
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:36:34 +0100

    Update RmatCustomizedSimulation.java

--- src/SomeSimulations/RmatCustomizedSimulation.java
+++ src/SomeSimulations/RmatCustomizedSimulation.java
@@ -2,15 +2,15 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.RmatGenerator;
-
 import java.util.List;
 
+/**
+* Cette classe prepare une simulation personalisé d'un generateur Rmat 
+*/
 public class RmatCustomizedSimulation extends CustomizedSimulation {
 
     public RmatCustomizedSimulation(List<Idea> ideas, int nbrNodes, int nbrEdges, float a, float b, float c,Idea idea){
         super(new RmatGenerator(nbrNodes,nbrEdges,a,b,c,ideas),idea);
-
     }
-
 
 }
