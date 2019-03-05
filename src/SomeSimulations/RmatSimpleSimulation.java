575e0ee57f49ec1ce268d5619ef6dc8b23f3fb2f
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:35:41 +0100

    Update RmatSimpleSimulation.java

--- src/SomeSimulations/RmatSimpleSimulation.java
+++ src/SomeSimulations/RmatSimpleSimulation.java
@@ -2,18 +2,16 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.RmatGenerator;
-
 import java.util.List;
 
+/**
+* Cette classe prepare une simulation simple d'un generateur Rmat
+*/
 public class RmatSimpleSimulation extends Simulation{
-
 
     public RmatSimpleSimulation(List<Idea> ideas,int nbrNodes, int nbrEdges, float a, float b, float c){
         super(new RmatGenerator(nbrNodes,nbrEdges,a,b,c,ideas));
-
     }
-
-
 
     @Override
     public void customizingGeneration() {
