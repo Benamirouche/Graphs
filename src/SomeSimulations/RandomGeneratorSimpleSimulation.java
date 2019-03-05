ba68c81a11813275f3cdcf85427f69c40d59b954
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:35:06 +0100

    Update RandomGeneratorSimpleSimulation.java

--- src/SomeSimulations/RandomGeneratorSimpleSimulation.java
+++ src/SomeSimulations/RandomGeneratorSimpleSimulation.java
@@ -2,9 +2,11 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.RandomGraphGenerator;
-
 import java.util.List;
 
+/**
+* Cette classe prepare une simulation simple d'un generateur Aléatoire
+*/
 public class RandomGeneratorSimpleSimulation extends SimpleSimulation {
 
     public RandomGeneratorSimpleSimulation(int nbrNodes, List<Idea> ideas, float p){
