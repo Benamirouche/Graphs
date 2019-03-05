3bc35f89c16925152c6fca6febf7a3fc424874d7
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:38:23 +0100

    Update JgraphTKleinbergSmallWorldCustomizedSimulation.java

--- src/SomeSimulations/JgraphTKleinbergSmallWorldCustomizedSimulation.java
+++ src/SomeSimulations/JgraphTKleinbergSmallWorldCustomizedSimulation.java
@@ -2,9 +2,11 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.JgraphTKleinbergSmallWorldGenerator;
-
 import java.util.List;
 
+/**
+* Cette classe prepare une simulation personalisé d'un generateur TKleinbergSmallWorld de JgraphT  
+*/
 public class JgraphTKleinbergSmallWorldCustomizedSimulation extends CustomizedSimulation {
 
     public JgraphTKleinbergSmallWorldCustomizedSimulation(List<Idea> ideas, int n, int p, int q, int r,Idea idea){
