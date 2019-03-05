e2bed986dd94f7f6865abb59144a44a3ba0521f0
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 12:51:55 +0100

    Update JgraphTScaleFreeGenerator.java

--- src/mainPackage/generators/JgraphTScaleFreeGenerator.java
+++ src/mainPackage/generators/JgraphTScaleFreeGenerator.java
@@ -4,9 +4,13 @@ import mainPackage.Idea;
 import mainPackage.Node;
 import org.jgrapht.generate.ScaleFreeGraphGenerator;
 import org.jgrapht.graph.DefaultEdge;
-
 import java.util.List;
 
+/**
+* <p>
+*   Cette Classe adapt le generateur ScaleFree de JgraphT a nos classes de Node et Edge
+*</p>
+*/
 public class JgraphTScaleFreeGenerator extends JgraphTGenerators{
     public JgraphTScaleFreeGenerator(List<Idea> ideas, int numberOfNodes) {
         super(ideas,new ScaleFreeGraphGenerator<Node, DefaultEdge>(numberOfNodes),numberOfNodes);
