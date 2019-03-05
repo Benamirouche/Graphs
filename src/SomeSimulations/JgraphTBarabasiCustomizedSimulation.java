09fbd92dc391ea0929d8a0d7e5299ff692c8c94c
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:37:19 +0100

    Update JgraphTBarabasiCustomizedSimulation.java

--- src/SomeSimulations/JgraphTBarabasiCustomizedSimulation.java
+++ src/SomeSimulations/JgraphTBarabasiCustomizedSimulation.java
@@ -2,9 +2,11 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.JgraphTBarabasiAlbertGenerator;
-
 import java.util.List;
 
+/**
+* Cette classe prepare une simulation personalisé d'un generateur Barabasi de JgraphT  
+*/
 public class JgraphTBarabasiCustomizedSimulation extends CustomizedSimulation {
 
 
