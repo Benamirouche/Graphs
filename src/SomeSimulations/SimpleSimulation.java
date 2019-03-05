77defeec8a83f57500924844a3e598002a3a0b0a
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:42:18 +0100

    Update SimpleSimulation.java

--- src/SomeSimulations/SimpleSimulation.java
+++ src/SomeSimulations/SimpleSimulation.java
@@ -1,13 +1,13 @@
 package SomeSimulations;
 
 import mainPackage.generators.BaseGenerator;
-
+/**
+* Cette classe represente une simulation simple, ça veut dire qu'on affecte aléatoirement des idée aux noeuds du graphe
+*/
 public class SimpleSimulation extends  Simulation {
 
 public SimpleSimulation(BaseGenerator generator){
     super(generator);
-
-
 }
 
     @Override
