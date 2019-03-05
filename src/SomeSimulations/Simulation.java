97d890d829f80df7ac8320dc6e16ba6e03d7c0ea
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:46:16 +0100

    Update Simulation.java

--- src/SomeSimulations/Simulation.java
+++ src/SomeSimulations/Simulation.java
@@ -1,51 +1,42 @@
 package SomeSimulations;
 
 import mainPackage.generators.BaseGenerator;
-
+/**
+* Cette classe est la classe mère des types de simulation, elle représente une simulation de génération de graphe
+* tout en déssinant les graphes de disrtibution des degres et en ecrivant les graphes dans des fichiers
+*/
 public abstract class Simulation {
 
     protected BaseGenerator generator;
 
     protected Simulation(BaseGenerator generator){
-
         this.generator=generator;
-
-
-
     }
 
     public void  generate(){
-
         generator.generate();
     }
 
     public void powerLawVerification(){
-
         generator.powerLawVerification();
     }
     public void drawPowerLaw(){
-
-
         generator.drawPowerLaw();
     }
 
     public void writeGexf(){
         generator.writeGexfGraph();
-
     }
     public  void writeStructuredGraph(){
-
         generator.writeStructuredGraph();
     }
 
     public void writeIdeas(){
-
        generator.writeIdeasApparition();
     }
     abstract public void customizingGeneration();
 
  public void simulate(){
-
         generate();
         customizingGeneration();
         powerLawVerification();
@@ -53,8 +44,6 @@ public abstract class Simulation {
         writeGexf();
         writeStructuredGraph();
         writeIdeas();
-
-
  }
 
 
