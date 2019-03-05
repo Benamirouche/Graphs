327ee13d91fac9d0dbeb33651ef136f700959a38
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 12:42:48 +0100

    Update JgraphTBarabasiAlbertGenerator.java

--- src/mainPackage/generators/JgraphTBarabasiAlbertGenerator.java
+++ src/mainPackage/generators/JgraphTBarabasiAlbertGenerator.java
@@ -4,24 +4,18 @@ import mainPackage.Idea;
 import mainPackage.Node;
 import org.jgrapht.generate.BarabasiAlbertGraphGenerator;
 import org.jgrapht.graph.DefaultEdge;
-
 import java.util.List;
 
+/**
+* <p>
+*   Cette Class adapt le generateur Barabasi de JgraphT a nos classes de Node et Edge
+*</p>
+*/
 public class JgraphTBarabasiAlbertGenerator extends JgraphTGenerators {
-
-
     public JgraphTBarabasiAlbertGenerator(List<Idea> ideas,int initialNumberOfNodes,int numberOfEdgesAddedPerNode,int finalNumberOfNodes){
         super(ideas,new BarabasiAlbertGraphGenerator<Node, DefaultEdge>(initialNumberOfNodes,numberOfEdgesAddedPerNode,finalNumberOfNodes),finalNumberOfNodes);
-
-
-
-
     }
     public JgraphTBarabasiAlbertGenerator(List<Idea> ideas,int initialNumberOfNodes,int numberOfEdgesAddedPerNode,int finalNumberOfNodes,long seed){
         super(ideas,new BarabasiAlbertGraphGenerator<Node, DefaultEdge>(initialNumberOfNodes,numberOfEdgesAddedPerNode,finalNumberOfNodes,seed),finalNumberOfNodes);
-
-
-
-
     }
 }
