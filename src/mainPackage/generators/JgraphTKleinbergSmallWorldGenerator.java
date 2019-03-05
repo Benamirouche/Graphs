479ed3a811e76254adc342c0fc731367828563f2
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 12:49:55 +0100

    Update JgraphTKleinbergSmallWorldGenerator.java

--- src/mainPackage/generators/JgraphTKleinbergSmallWorldGenerator.java
+++ src/mainPackage/generators/JgraphTKleinbergSmallWorldGenerator.java
@@ -6,17 +6,15 @@ import org.jgrapht.generate.KleinbergSmallWorldGraphGenerator;
 import org.jgrapht.graph.DefaultEdge;
 
 import java.util.List;
+/**
+* <p>
+*   Cette Class adapte le generateur KleinbergSmallWorld de JgraphT a nos classes de Node et Edge
+*</p>
+*/
 
 public class JgraphTKleinbergSmallWorldGenerator extends  JgraphTGenerators{
-
-
-
-
 
     public JgraphTKleinbergSmallWorldGenerator(List<Idea> ideas,int n,int p,int q,int r) {
         super(ideas,new KleinbergSmallWorldGraphGenerator<Node, DefaultEdge>(n,p,q,r));
     }
-
-
-
 }
