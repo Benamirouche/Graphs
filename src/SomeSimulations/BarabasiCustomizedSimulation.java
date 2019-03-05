a484cde0bed5cb66683756ebdf97dfec2434782b
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:29:46 +0100

    Update BarabasiCustomizedSimulation.java

--- src/SomeSimulations/BarabasiCustomizedSimulation.java
+++ src/SomeSimulations/BarabasiCustomizedSimulation.java
@@ -2,23 +2,18 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.BarabasiGenerator;
-
 import java.util.List;
-
+/**
+* la classe qui prepare une simulation personalisé d'un generateur Barabasi
+*/
 public class BarabasiCustomizedSimulation extends CustomizedSimulation {
-
-
-
-
 
     public BarabasiCustomizedSimulation(float randomRatio, List<Idea> ideas, int finalNbrNodes , int k,Idea idea){
         super(new BarabasiGenerator(randomRatio,ideas,finalNbrNodes,k),idea);
-
     }
 
     public BarabasiCustomizedSimulation(float randomRatio, List<Idea> ideas, int finalNbrNodes , int k,float randomDensity,Idea idea){
         super(new BarabasiGenerator(randomRatio,ideas,finalNbrNodes,k,randomDensity),idea);
-
     }
 
 }
