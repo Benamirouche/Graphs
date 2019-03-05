acf51752ce7996d1a9e0152276ae07ceb0466f83
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:40:24 +0100

    Update JgraphTScaleFreeSimpleSimulation.java

--- src/SomeSimulations/JgraphTScaleFreeSimpleSimulation.java
+++ src/SomeSimulations/JgraphTScaleFreeSimpleSimulation.java
@@ -2,9 +2,11 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.JgraphTScaleFreeGenerator;
-
 import java.util.List;
 
+/**
+* Cette classe prepare une simulation simple d'un generateur ScalFree de JgraphT
+*/
 public class JgraphTScaleFreeSimpleSimulation extends SimpleSimulation{
 
     public JgraphTScaleFreeSimpleSimulation(List<Idea> ideas,int nbrOfNodes){
