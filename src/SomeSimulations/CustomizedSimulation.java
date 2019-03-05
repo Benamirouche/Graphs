a4400d82732462796072bcf7f2339ff0b7bee367
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:32:51 +0100

    Update CustomizedSimulation.java

--- src/SomeSimulations/CustomizedSimulation.java
+++ src/SomeSimulations/CustomizedSimulation.java
@@ -3,11 +3,14 @@ package SomeSimulations;
 import mainPackage.Idea;
 import mainPackage.Node;
 import mainPackage.generators.BaseGenerator;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+* Cette classe represente une simulation personalisé, tel qu'on affecte à un noeud infuent une idée differente des autres pour
+* observer la propagation de cette idée dans le réseau.
+*/
 public class CustomizedSimulation extends Simulation {
 protected Idea idea;
 protected CustomizedSimulation(BaseGenerator generator, Idea idea){
@@ -19,20 +22,8 @@ protected CustomizedSimulation(BaseGenerator generator, Idea idea){
     public void customizingGeneration() {
         Optional<Map.Entry<Node, List<Node>>> mostFollowedNodeEntry=generator.getMostFollowedNode();
         mostFollowedNodeEntry.ifPresent(entry->{
-
-
-            System.out.println("***************"+entry.getKey()+" avec "+entry.getValue().size()+" followers *****");
-            System.out.println("************");
-
             Node mostFollowedNode=entry.getKey();
             generator.customizeGraph(mostFollowedNode,idea);
-
-
-
         });
-
     }
-
-
-
 }
