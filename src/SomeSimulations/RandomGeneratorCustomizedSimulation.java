60ddd25d4f7b396df577e70363e6ece473abe16e
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:34:28 +0100

    Update RandomGeneratorCustomizedSimulation.java

--- src/SomeSimulations/RandomGeneratorCustomizedSimulation.java
+++ src/SomeSimulations/RandomGeneratorCustomizedSimulation.java
@@ -2,17 +2,14 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.RandomGraphGenerator;
-
 import java.util.List;
 
+/**
+* Cette classe prepare une simulation presonalisé d'un generateur aléatoire
+*/
 public class RandomGeneratorCustomizedSimulation extends CustomizedSimulation{
     public RandomGeneratorCustomizedSimulation(int nbrNodes, List<Idea> ideas, float p,Idea idea)
     {
         super(new RandomGraphGenerator(nbrNodes,ideas,p),idea);
-
-
     }
-
-
-
 }
