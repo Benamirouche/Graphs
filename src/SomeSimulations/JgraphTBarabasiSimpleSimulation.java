9759a764f7e4c0830d1e4250f6e4faed795f495e
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:37:41 +0100

    Update JgraphTBarabasiSimpleSimulation.java

--- src/SomeSimulations/JgraphTBarabasiSimpleSimulation.java
+++ src/SomeSimulations/JgraphTBarabasiSimpleSimulation.java
@@ -2,9 +2,11 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.JgraphTBarabasiAlbertGenerator;
-
 import java.util.List;
 
+/**
+* Cette classe prepare une simulation simple d'un generateur Barabasi de JgraphT  
+*/
 public class JgraphTBarabasiSimpleSimulation extends  SimpleSimulation{
 
     public JgraphTBarabasiSimpleSimulation(List<Idea> ideas, int initialNumberOfNodes, int numberOfEdgesAddedPerNode, int finalNumberOfNodes){
