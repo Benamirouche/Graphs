e901e31b399576ca4158f6ec94d329fdab9cb38c
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:39:55 +0100

    Update JgraphTScaleFreeCustomizedSimulation.java

--- src/SomeSimulations/JgraphTScaleFreeCustomizedSimulation.java
+++ src/SomeSimulations/JgraphTScaleFreeCustomizedSimulation.java
@@ -2,15 +2,15 @@ package SomeSimulations;
 
 import mainPackage.Idea;
 import mainPackage.generators.JgraphTScaleFreeGenerator;
-
 import java.util.List;
 
+/**
+* Cette classe prepare une simulation personalisé d'un generateur ScalFree de JgraphT
+*/
 public class JgraphTScaleFreeCustomizedSimulation extends CustomizedSimulation{
-
 
     public JgraphTScaleFreeCustomizedSimulation(List<Idea> ideas, int nbrOfNodes,Idea idea){
         super(new JgraphTScaleFreeGenerator(ideas,nbrOfNodes),idea);
     }
-
 
 }
