d16007e91803fc676dccfad4ea1e85b684be2633
Benamirouche Abdelmalek <45042407+Benamirouche@users.noreply.github.com>
2019-03-05 13:08:42 +0100

    Update GraphWriter.java

--- src/mainPackage/GraphWriters/GraphWriter.java
+++ src/mainPackage/GraphWriters/GraphWriter.java
@@ -1,33 +1,52 @@
 package mainPackage.GraphWriters;
 
 import mainPackage.generators.BaseGenerator;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
+ 
+/**
+* Cette classe est la classe mère de tous les classes responsables de l'écriture des graphes dans des fichiers,
+* les classes filles doivent implémenter la methode write()
+*/
 public abstract class GraphWriter {
+    /**
+    * le chemin de base vers le repertoire des fichers d'ecritures
+    */
     protected static final String BASE_PATH=".."+File.separator+
                                             "ProjectFiles"+ File.separator;
-
+    /**
+    * le chemin de base vers le ficher representant graphe
+    */
     protected static final String GRAPHS_PATH=BASE_PATH+"graphs"+File.separator;
 
     protected BufferedWriter bw=null;
 
     protected FileWriter fw=null;
-
+    /**
+    * le graphe à écrire dans le fichier
+    */
     protected BaseGenerator graphGenerator;
-
+    
+    /**
+    * le constructeur du GraphWriter
+    */
     public GraphWriter(BaseGenerator graphGenerator){
         this.graphGenerator=graphGenerator;
     }
 
+    /**
+    * la methode chargée de l'ouverture du fichier d'écriture
+    */
     public void openFile(String name) throws IOException {
         fw = new FileWriter(GRAPHS_PATH+name);
         bw=new BufferedWriter(fw);
     }
 
+    /**
+    * la methode chargée de la fermeture du fichier d'écriture
+    */
     public void close() throws IOException {
         if(bw!=null)
             bw.close();
@@ -36,5 +55,8 @@ public abstract class GraphWriter {
 
     }
 
+    /**
+    * la methode chargée de l'ecriture du graphe dans fichier d'écriture
+    */
     public abstract void write() throws IOException;
 }
